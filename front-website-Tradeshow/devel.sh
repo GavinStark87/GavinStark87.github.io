f7a7687eb3b60e7c14208ec92372a62e00879349
GavinStark87 <56038381+GavinStark87@users.noreply.github.com>
2019-11-08 09:04:16 -0800

    New grouping

---
+++ front-website-Tradeshow/devel.sh
@@ -0,0 +1,11 @@
+rm -r ejsout/*
+
+cp -r img/ ejsout/img
+cp -r js/ ejsout/js
+cp -r scss/ ejsout/scss
+cp -r fonts/ ejsout/fonts
+cp -r vendors/ ejsout/vendors
+cp -r css/ ejsout/css 
+
+ejs-cli -f index.ejs -o ejsout/
+ejs-cli -f elements/index.ejs -o ejsout/
---
+++ front-website-Tradeshow/live.sh
@@ -0,0 +1,17 @@
+rm -r ejsout/*
+
+cp -r img/ ejsout/img
+cp -r js/ ejsout/js
+cp -r scss/ ejsout/scss
+cp -r fonts/ ejsout/fonts
+cp -r vendors/ ejsout/vendors
+cp -r css/ ejsout/css 
+
+ejs-cli -f index.ejs -o ejsout/
+ejs-cli -f elements/index.ejs -o ejsout/
+
+echo "Pushing to Google now..."
+
+cd ejsout/
+
+gsutil -m cp -r * gs://evaapp.xyz
